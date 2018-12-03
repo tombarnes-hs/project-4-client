92e591ec44e661492440b6c3cc9b03263f127d75
Tom Guncik <tom.guncik@gmail.com>
2018-12-03 12:06:21 -0500

    Add deleteMovie function to Ship component
    - build button with onClick value of this.deleteMovie
    - deleteMovie performs a Delete HTTP request to AIP with axios
    - User redirected to Ships component after successful deletion

--- src/body/components/Ship.js
+++ src/body/components/Ship.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { Redirect } from 'react-router-dom'
 import axios from 'axios'
 
 const apiUrl = 'http://localhost:4741/ships'
@@ -12,8 +12,10 @@ class Ship extends React.Component {
     console.log({id})
 
     this.state = {
-      ship: {}
+      ship: {},
+      delete: false
     }
+    this.deleteShip = this.deleteShip.bind(this)
   }
 
   async componentDidMount() {
@@ -27,18 +29,34 @@ class Ship extends React.Component {
     this.setState({ship: response.data.ship})
   }
 
+  async deleteShip() {
+    const config = {
+      headers: {
+        'Authorization': `Token token=${this.props.user.token}`
+      }
+    }
+    const id = this.props.match.params.id
+    const response = await axios.delete(apiUrl + `/${id}`, config)
+    this.setState({deleted: true})
+  }
+
 
   render() {
-    console.log(this.state)
+    if (this.state.deleted === true) {
+      return <Redirect to='/ships' />
+    }
     const { ship } = this.state
     const { name, pilot, notes } = ship
     return (
       <React.Fragment>
         <h1>Unit Info</h1>
-        <h2>{ship ? name : 'Loading'}</h2>
-        <h3>{ship ? `Favorite Pilot: ${pilot}` : ''}</h3>
-        <p>{ship ? `Notes: ${notes}` : ''}</p>
+        <h3>{ship ? name : 'Loading'}</h3>
+        <hr></hr>
+        <br></br>
 
+        <h4>{ship ? `Favorite Pilot: ${pilot}` : ''}</h4>
+        <p>{ship ? `Notes: ${notes}` : ''}</p>
+        <button onClick={this.deleteShip}>Remove from Collection</button>
       </React.Fragment>
     )
   }
