e81c0697d606930e5198f412bdee16295c941c2c
Tom Guncik <tom.guncik@gmail.com>
2018-12-06 10:39:14 -0500

    Delete old, commented out code Dashboard.js

--- src/body/components/Dashboard.js
+++ src/body/components/Dashboard.js
@@ -47,36 +47,4 @@ const Dashboard = ({ user }) => (
   </React.Fragment>
 )
 
-// class Dashboard extends React.Component {
-
-//   render() {
-//     return (
-//       <div>
-//         <h1>Player dashboard</h1>
-//
-//         <button><Link to="/ships">View Owned Ships</Link></button>
-//         <button><Link to="/ships/create">Add Ship</Link></button>
-//
-//         <Router>
-//           <Switch>
-//
-//             <Route exact path="/ships" render={(props) => (
-//               <Ships user={this.props.user} {...props}/>)} />
-//
-//             <Route exact path="/ships/create" render={() => (
-//               <ShipCreate user={this.props.user} ship={this.state}/>)} />
-//
-//             <Route exact path="/ships/:id" render={(props) => (
-//               <Ship user={this.props.user} {...props}/>)} />
-//
-//             <Route exact path="/ships/:id/update" render={(props) => (
-//               <ShipUpdate user={this.props.user} {...props}/>)} />
-//
-//           </Switch>
-//         </Router>
-//       </div>
-//     )
-//   }
-// }
-
 export default Dashboard
